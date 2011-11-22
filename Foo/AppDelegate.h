43710a0a443454b5db3ca7b5f60cf9eb2a143a43
Muhammad Taufik <mailtaufik@gmail.com>
2011-11-22 16:43:16 +0700

    Initial Commit

---
+++ Foo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Foo
+//
+//  Created by Muhammad Taufik on 11/22/11.
+//  Copyright (c) 2011 Beetlebox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ Foo/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Foo
+//
+//  Created by Muhammad Taufik on 11/22/11.
+//  Copyright (c) 2011 Beetlebox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
